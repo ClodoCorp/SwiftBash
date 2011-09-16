d20090414d424f4e5db6efcacd498b55b6753c65
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2011-09-17 02:52:13 +0400

    Basic Auth function added

--- uploader.sh
+++ uploader.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+. swiftlib.sh
 
-AUTH_URL='http://testapi.clodo.ru/v1'
 STORAGE_USER='storage_6681_1'
 STORAGE_KEY='857a63980c5b3640f3053cdb80156b7e'
-API_URL=""
-API_TOKEN=""
+STORAGE_KEY='ad4f23431fbd68512cd0d8929443baaa'
 
 DIR="$1"
 CNT="$2"
@@ -14,35 +13,8 @@ if [ -z "$DIR" -o -z "$CNT" ]; then
     exit 1
 fi
 
-function auth() {
-    AUTH_DAT="$(curl -I -s -H "X-Auth-User: $STORAGE_USER" -H "X-Auth-Key: $STORAGE_KEY" $AUTH_URL)"
 
-    if [ -z "$AUTH_DAT" ]; then
-        echo "ERROR: Can't find Cloud Storage server"
-        exit 1
-    fi
-
-    ERR=`echo "$AUTH_DAT" | grep 'Unauthorized'`
-    if [ $? -eq 0 ]; then
-        echo "ERROR: Unauthorized!"
-        exit 1
-    fi
-
-    API_URL=`echo "$AUTH_DAT" | grep 'X-Storage-Url'|sed 's/X-Storage-Url: \(.*\)\r/\1/'`
-    if [ -z "$API_URL" ]; then
-        echo "ERROR: Error getting API URL"
-        exit 1
-    fi
-
-    API_TOKEN=`echo "$AUTH_DAT" | grep 'X-Storage-Token' | sed 's/X-Storage-Token: \(.*\)\r/\1/'`
-    if [ -z "$API_TOKEN" ]; then
-        echo "ERROR: Error getting API TOKEN"
-        exit 1
-    fi
-}
-
-auth
-
+authenticate $STORAGE_USER $STORAGE_KEY
 
 for dir in `find $DIR -type d`
 do
@@ -50,10 +22,9 @@ do
     echo $FNM
     curl -f -X PUT -s -H "X-Storage-Token: $API_TOKEN" -H "Content-Type: application/directory" -H "Content-Length: 0" $API_URL/$CNT/$FNM > /dev/null
     if [ "$?" -eq "22" ]; then
-        auth
+        authenticate $STORAGE_USER $STORAGE_KEY
         curl -X PUT -s -H "X-Storage-Token: $API_TOKEN" -H "Content-Type: application/directory" -H "Content-Length: 0" $API_URL/$CNT/$FNM > /dev/null    
     fi
-
 done
 
 for file in `find $DIR`
@@ -62,7 +33,7 @@ do
     echo $FNM
     curl -f -I -s -T $file -H "X-Storage-Token: $API_TOKEN" $API_URL/$CNT/$FNM > /dev/null
     if [ "$?" -eq "22" ]; then
-        auth
+        authenticate $STORAGE_USER $STORAGE_KEY
         curl -I -s -T $file -H "X-Storage-Token: $API_TOKEN" $API_URL/$CNT/$FNM > /dev/null
     fi
 done
